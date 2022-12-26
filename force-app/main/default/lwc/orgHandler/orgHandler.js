714d66fff076da3b57f1a8fc4c3ae2e4f1be8d12
Bhaskar Mahto <arbkm22@gmail.com>
2022-12-26 21:41:12 +0530

    - added aura enabled annotation to apex class - added js to fetch data through wire

--- force-app/main/default/lwc/orgHandler/orgHandler.js
+++ force-app/main/default/lwc/orgHandler/orgHandler.js
@@ -1,10 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import getTriggers from '@salesforce/apex/orgHandlerClass.getTriggers';
+
 import noHeader from '@salesforce/resourceUrl/NoHeader';
 import { loadStyle } from 'lightning/platformResourceLoader';
 
 export default class OrgHandler extends LightningElement {
+    
+    TriggerNameList = [];
+    EnumOrIdList = [];
+    ApiVersionList = [];
+    
     connectedCallback() {
         loadStyle(this, noHeader)
             .then(result => {});
+    }
+
+    @wire(getTriggers) getTriggersList({ data, error }) {
+        if (data)
+            console.log(data);
+        else if (error) 
+            console.log(error);
     }
 }
