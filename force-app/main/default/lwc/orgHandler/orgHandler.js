48853cdcb929bb21aa4832ed8cfbbadbe51446be
Bhaskar Mahto <arbkm22@gmail.com>
2022-12-26 18:57:51 +0530

    first commit

--- force-app/main/default/lwc/orgHandler/orgHandler.js
+++ force-app/main/default/lwc/orgHandler/orgHandler.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
+import noHeader from '@salesforce/resourceUrl/NoHeader';
+import { loadStyle } from 'lightning/platformResourceLoader';
 
-export default class OrgHandler extends LightningElement {}
+export default class OrgHandler extends LightningElement {
+    connectedCallback() {
+        loadStyle(this, noHeader)
+            .then(result => {});
+    }
+}
