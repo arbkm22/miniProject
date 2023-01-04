9d23c2fcf8f6b1f294574ccc346b465ef32323d3
Bhaskar Mahto <arbkm22@gmail.com>
2023-01-04 09:26:09 +0530

    updated according to new schema

--- force-app/main/default/lwc/orgHandler/orgHandler.js
+++ force-app/main/default/lwc/orgHandler/orgHandler.js
@@ -1,24 +1,64 @@
 import { LightningElement, track, wire } from 'lwc';
-import getTriggers from '@salesforce/apex/orgHandlerClass.getTriggers';
+import { refreshApex } from '@salesforce/apex';
+import getTriggerList from '@salesforce/apex/orgHandler.getTriggerList';
+import updateTrigger from '@salesforce/apex/orgHandler.updateTrigger';
 
 import noHeader from '@salesforce/resourceUrl/NoHeader';
 import { loadStyle } from 'lightning/platformResourceLoader';
 
 export default class OrgHandler extends LightningElement {
-    
-    TriggerNameList = [];
-    EnumOrIdList = [];
-    ApiVersionList = [];
-    
+
     connectedCallback() {
         loadStyle(this, noHeader)
             .then(result => {});
     }
 
-    @wire(getTriggers) getTriggersList({ data, error }) {
-        if (data)
-            console.log(data);
-        else if (error) 
-            console.log(error);
+    @track
+    triggers;
+
+    wiredApexResult;
+
+    @track
+    columns = [
+        { label: 'Name', fieldName: 'Name' },
+        { label: 'Object', fieldName: 'Object__c' },
+        {label: 'Status', fieldName: 'Status__c'},
+        {
+            type:'button',
+            fixedWidth: 150,
+            typeAttributes: {
+                label: 'Edit',
+                name: 'edit',
+                variant: 'brand'
+            }
+        }
+    ];
+
+    @wire(getTriggerList)
+    getTriggersList(result) {
+        this.wiredApexResult = result;
+        if (result.data) {
+            console.log(result.data);
+            this.triggers = result.data;            
+        }
+        else if (result.error) {
+            console.log(result.error);
+        }
     }
+
+    updateTrigger(event) {
+        let tgName = event.detail.row.Name;
+        let tgObject = event.detail.row.Object__c;
+        this.objectName = event.detail.row.Object__c;
+        console.log(tgName + ' ' + tgObject);
+        updateTrigger({objectName: tgObject})
+        .then(() => {
+            console.log("updated successfully");
+            return refreshApex(this.wiredApexResult);
+        })
+        .catch(() => {
+            console.log("error");
+        });
+    }   
+    
 }
