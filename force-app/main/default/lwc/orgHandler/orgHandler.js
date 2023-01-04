6b722f535ab5d96308fbc2c1d08ebd2e28b2556b
Bhaskar Mahto <arbkm22@gmail.com>
2023-01-04 14:05:53 +0530

    added hyperlink in name field

--- force-app/main/default/lwc/orgHandler/orgHandler.js
+++ force-app/main/default/lwc/orgHandler/orgHandler.js
@@ -2,7 +2,7 @@ import { LightningElement, track, wire } from 'lwc';
 import { refreshApex } from '@salesforce/apex';
 import getTriggerList from '@salesforce/apex/orgHandler.getTriggerList';
 import updateTrigger from '@salesforce/apex/orgHandler.updateTrigger';
-
+import LightningConfirm from 'lightning/confirm';
 import noHeader from '@salesforce/resourceUrl/NoHeader';
 import { loadStyle } from 'lightning/platformResourceLoader';
 
@@ -13,16 +13,16 @@ export default class OrgHandler extends LightningElement {
             .then(result => {});
     }
 
-    @track
-    triggers;
-
+    @track triggers;
+    @track isLoading = false;
     wiredApexResult;
 
     @track
     columns = [
-        { label: 'Name', fieldName: 'Name' },
+        { label: 'Name', fieldName: 'TriggerName', type: 'url',
+            typeAttributes: { label: { fieldName: 'Name'}, target: '_blank'} },
         { label: 'Object', fieldName: 'Object__c' },
-        {label: 'Status', fieldName: 'Status__c'},
+        { label: 'Status', fieldName: 'Status__c' },
         {
             type:'button',
             fixedWidth: 150,
@@ -39,26 +39,45 @@ export default class OrgHandler extends LightningElement {
         this.wiredApexResult = result;
         if (result.data) {
             console.log(result.data);
-            this.triggers = result.data;            
+            let tempRecs = [];
+            const resultData = Array.from(result.data);
+            resultData.forEach( (record) => {
+                let tempRec = Object.assign( {}, record );
+                tempRec.TriggerName = '/' + tempRec.Id;
+                tempRecs.push(tempRec);
+            });
+            this.triggers = tempRecs;            
         }
         else if (result.error) {
             console.log(result.error);
         }
     }
 
-    updateTrigger(event) {
+    async updateTrigger(event) {
         let tgName = event.detail.row.Name;
         let tgObject = event.detail.row.Object__c;
+        let tgStatus = event.detail.row.Status__c;
+        let tgConfirm = (tgStatus == 'Active') ? 'Deactivate' : 'Activate';
         this.objectName = event.detail.row.Object__c;
         console.log(tgName + ' ' + tgObject);
-        updateTrigger({objectName: tgObject})
-        .then(() => {
-            console.log("updated successfully");
-            return refreshApex(this.wiredApexResult);
-        })
-        .catch(() => {
-            console.log("error");
+        const result = await LightningConfirm.open({
+            message: 'Are you sure you want to ' + tgConfirm + ' ' + tgName + '?',
+            variant: 'default',
+            label: 'Change trigger status'
         });
+        if (result) {
+            this.isLoading = true;
+            updateTrigger({objectName: tgObject})
+            .then(() => {
+                console.log("updated successfully");
+                this.isLoading = false;
+                return refreshApex(this.wiredApexResult);
+            })
+            .catch(() => {
+                console.log("error");
+            });
+        }
+        
     }   
     
 }
